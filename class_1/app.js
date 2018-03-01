dbc20a77063a3438bf923d22c7a23e434f05b3af
darking09 <ftorres@mdsdigital.com>
2018-03-01 13:17:50 -0500

    Clase 1

---
+++ class_1/app.js
@@ -0,0 +1,17 @@
+/*var nombreApellido = "Niño Camilo";
+var estatura = 150.5;
+var soltero = true; // false
+var programadores = ['Cristian', 2, 'Gonzalo', 'Jose', 'Fernando'];*/
+
+
+var print =  document.getElementById('root');
+var programadores = 'Pepe no es nadie';
+
+
+if (true) {
+	console.log('Imprima dos '+programadores);	
+}
+
+console.log(programadores.length);
+
+print.innerHTML = '<h1 class="nombre" >Clase de Javascript I</h1>';
